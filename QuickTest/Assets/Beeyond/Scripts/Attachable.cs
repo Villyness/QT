b2fa5eb0383a1305297a2860c37a52b91524a295
Venus <inkyinked@gmail.com>
2019-02-28 15:24:22 +1100

    Tested for flower attachment: Sucess

---
+++ QuickTest/Assets/Beeyond/Scripts/Attachable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Attachable : MonoBehaviour
+{
+    public GameObject ObjectToAttachTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<Trigger>().Landing += Attach;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Attach(Vector3 AttachPos)
+    {
+        transform.position = AttachPos;
+        this.transform.parent = ObjectToAttachTo.transform;
+    }
+}
---
+++ QuickTest/Assets/Beeyond/Scripts/Trigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Trigger : MonoBehaviour
+{
+    public event Action<Vector3> Landing;
+    public Vector3 OwnPos;
+
+    void Start()
+    {
+        OwnPos = transform.position;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Landing(OwnPos);
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Landing(OwnPos);
+        }
+        Debug.Log("Hello World");
+    }
+}
